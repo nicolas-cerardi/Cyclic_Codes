2f3f655d329d0ce45a8d8e70f8422d0ace3c6e9c
ncerardi <None>
2018-09-13 11:54:55 +0200

    FFPoly debugged and working

--- HammingCodes/FFPoly.py
+++ HammingCodes/FFPoly.py
@@ -10,12 +10,12 @@ def sanity_pol_check(x, fieldcardinal):
 
 class FFPoly:
 
-    def __init__(array, fieldcardinal):
+    def __init__(self, array, fieldcardinal):
         array = sanity_pol_check(array, fieldcardinal)
         self.fieldcardinal = fieldcardinal
         self.coeffs = array % fieldcardinal
 
-    def __mul__(y):
+    def __mul__(self, y):
         assert self.fieldcardinal == y.fieldcardinal
         final_len = len(y.coeffs) + len(self.coeffs)
         res = np.zeros((final_len))
@@ -24,40 +24,54 @@ class FFPoly:
                 res[i + j] += cox * coy
         return FFPoly(res, self.fieldcardinal)
 
-    def __add__(y):
+    def __add__(self, y):
         assert self.fieldcardinal == y.fieldcardinal
         rescoeff = np.zeros((max((len(self.coeffs), len(y.coeffs)))))
         rescoeff[:len(self.coeffs):] += self.coeffs
         rescoeff[:len(y.coeffs):] += y.coeffs
         return FFPoly(rescoeff, self.fieldcardinal)
 
-    def __sub__(y):
+    def __sub__(self, y):
         assert self.fieldcardinal == y.fieldcardinal
         rescoeff = np.zeros((max((len(self.coeffs), len(y.coeffs)))))
         rescoeff[:len(self.coeffs):] -= self.coeffs
         rescoeff[:len(y.coeffs):] -= y.coeffs
         return FFPoly(rescoeff, self.fieldcardinal)
 
-    def __div__(y):
+    def __div__(self, y):
         assert self.fieldcardinal == y.fieldcardinal
         dividend = sanity_pol_check(self.coeffs, self.fieldcardinal)
         divisor = sanity_pol_check(y.coeffs, y.fieldcardinal)
         if len(divisor) > len(dividend):
-            quotient, remainder = FFPoly(np.zeros((1))), self
+            quotient, remainder = FFPoly(np.zeros((1)), self.fieldcardinal), self
             return quotient, remainder
 
         else:
             maincoeffx, maincoeffy = dividend[-1], divisor[-1]
             for k in range(self.fieldcardinal):
                 if (maincoeffy * k) % self.fieldcardinal == maincoeffx:
-                    quotient = np.zeros((len(dividend) - len(divisor)))
+                    quotient = np.zeros((len(dividend) - len(divisor) + 1))
                     quotient[-1] = k
                     substract = np.zeros((len(dividend)))
                     substract[-len(divisor)::] = k * divisor
                     next_div = FFPoly(dividend - substract ,self.fieldcardinal).coeffs
-                    next_quot, remainder = FFPoly(next_div, self.fieldcardinal).__div__(divisor)
-                    quotient = FFPoly(quotient, self.fieldcardinal).__add__(FFPoly(next_quot, self.fieldcardinal))
-                    return quotient.coeffs, remainder
+                    next_quot, remainder = FFPoly(next_div, self.fieldcardinal).__div__(FFPoly(divisor, self.fieldcardinal))
+                    quotient = FFPoly(quotient, self.fieldcardinal).__add__(FFPoly(next_quot.coeffs, self.fieldcardinal))
+                    return quotient, remainder
 
         errmsg = 'this Division failed : ' + str(dividend) + ' | ' + str(divisor)
         raise ValueError(errmsg)
+
+
+# to test the class
+if __name__=='__main__' :
+    a = np.array([1,3,0,0,0,1,1])
+    b = np.array([1,0,1,0,1])
+    c = np.array([0,0,1,0,0])
+    ap, bp, cp = FFPoly(a, 2), FFPoly(b,2), FFPoly(c,2)
+    print((ap + bp).coeffs)
+    print((bp - ap).coeffs)
+    print((ap * cp).coeffs)
+    quot, rem = (ap / cp)
+    print("Quot : ", quot.coeffs)
+    print("Rem : ", rem.coeffs)
