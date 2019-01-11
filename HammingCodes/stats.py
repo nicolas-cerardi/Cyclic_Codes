4ff4ca4533ade7f1e9a8d2f62757b668d75cc01f
Nicolas CERARDI <nicolas.cerardi@corp.sophia.mines-paristech.fr>
2019-01-11 16:30:48 +0100

    dvpt stats.py

--- HammingCodes/stats.py
+++ HammingCodes/stats.py
@@ -12,7 +12,7 @@ from util import transmission_score, dist_h
 from time import time
 
 NB_TEST = 20
-NB_LAMBDA = 50
+NB_LAMBDA = 20
 testtext = "Non mais moi, ces histoires de nord et de sud, j'aime pas trop ! Selon comment on est tournés, ça change tout !"
 
 def hypscale(arr, a=1.04, b=-1/20, c=-0.1):
@@ -25,14 +25,13 @@ def hypscale(arr, a=1.04, b=-1/20, c=-0.1):
     
     return res
 
-paramcoder = [(3,2)]
 lambdas = np.linspace(0,1,NB_LAMBDA)
 hyplambdas = hypscale(lambdas)
 lambdas = np.linspace(0.5, 0.999, NB_LAMBDA)
 
-def mkstats(lambdas, errfunc=dist_h):
+def mkstats(lambdas, errfunc=dist_h, paramcoder=(3,2)):
     inittime = time()  
-    crt_code = Coder(paramcoder[0][0], paramcoder[0][1], 0)
+    crt_code = Coder(paramcoder[0], paramcoder[1], 0)
     res = np.zeros((NB_LAMBDA))
     for i in range(NB_LAMBDA):  
         crt_res = 0
@@ -52,11 +51,25 @@ def mkstats(lambdas, errfunc=dist_h):
     return res
 
 if __name__ == '__main__' :
-    fig, axarr = plt.subplots(2,1)
+    res_dh_m3 = mkstats(lambdas)
+    res_ts_m3 = mkstats(lambdas, errfunc=transmission_score)
+    res_dh_m3 = res_dh_m3 / np.max(res_dh_m3)
+    res_ts_m3 = res_ts_m3 / np.max(res_ts_m3)
+    
+    res_dh_m4 = mkstats(lambdas, paramcoder=(4,2))
+    res_ts_m4 = mkstats(lambdas, errfunc=transmission_score, paramcoder=(4,2))
+    res_dh_m4 = res_dh_m4 / np.max(res_dh_m4)
+    res_ts_m4 = res_ts_m4 / np.max(res_ts_m4)
+    
+    fig, axarr = plt.subplots(1,2)
 
-    axarr[0].plot(lambdas, mkstats(lambdas), 'b')
-    axarr[1].plot(lambdas, mkstats(lambdas, errfunc=transmission_score), 'r')
+    axarr[0].plot(lambdas, res_dh_m3, 'b')
+    axarr[0].plot(lambdas, res_ts_m3, 'r')
     axarr[0].grid(linestyle='--')
+    
+    axarr[1].plot(lambdas, res_dh_m4, 'b')
+    axarr[1].plot(lambdas, res_ts_m4, 'r')
     axarr[1].grid(linestyle='--')
+    
     #plt.plot(hyplambdas, mkstats(hyplambdas), 'r')
     plt.show()
